7e206eeefb8f742ea5d847905008ee17ca8f806e
Steven Braham <steven@braham.biz>
2016-10-27 00:55:36 +0200

    common header

--- gulpfile.js
+++ gulpfile.js
@@ -19,7 +19,7 @@ gulp.task('sass', ['clean'], function () {
 
 gulp.task('copy', function () {
     gulp.src('source/js/**/*.js').pipe(gulp.dest('build/js'));
-    gulp.src('source/images/**/*.js').pipe(gulp.dest('build/images'));
+    gulp.src('source/images/**/*').pipe(gulp.dest('build/images'));
 });
 
 //file watcher
@@ -48,7 +48,54 @@ gulp.task('handlebars', function () {
     var options = {
         ignorePartials: true,
         batch: ['./source/partials'],
-        helpers: {}
+        helpers: {
+            compare: function (lvalue, rvalue, options) {
+
+                if (arguments.length < 3)
+                    throw new Error("Handlerbars Helper 'compare' needs 2 parameters");
+
+                var operator = options.hash.operator || "==";
+
+                var operators = {
+                    '==': function (l, r) {
+                        return l == r;
+                    },
+                    '===': function (l, r) {
+                        return l === r;
+                    },
+                    '!=': function (l, r) {
+                        return l != r;
+                    },
+                    '<': function (l, r) {
+                        return l < r;
+                    },
+                    '>': function (l, r) {
+                        return l > r;
+                    },
+                    '<=': function (l, r) {
+                        return l <= r;
+                    },
+                    '>=': function (l, r) {
+                        return l >= r;
+                    },
+                    'typeof': function (l, r) {
+                        return typeof l == r;
+                    }
+                }
+
+                if (!operators[operator])
+                    throw new Error("Handlerbars Helper 'compare' doesn't know the operator " + operator);
+
+                var result = operators[operator](lvalue, rvalue);
+
+                if (result) {
+                    return options.fn(this);
+                } else {
+                    return options.inverse(this);
+                }
+
+            }
+        }
     }
     return gulp.src('source/pages/*.html').pipe(handlebars(templateData, options)).pipe(gulp.dest('build'));
 });
